cec27da61374a444571548b53a632020f652b717
siri99 <kl_sirisha@yahoo.com>
2017-10-20 23:46:03 +0800

    updated tests

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -27,7 +27,6 @@ import seedu.address.logic.commands.HistoryCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.SelectCommand;
-import seedu.address.logic.commands.SortCommand;
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
