10ec6e8b713a7c6c9e99ef08c6ddf2b3ecfc8874
siri99 <kl_sirisha@yahoo.com>
2017-10-23 23:59:57 +0800

    Updated AddressBookTest

--- src/test/java/seedu/address/model/AddressBookTest.java
+++ src/test/java/seedu/address/model/AddressBookTest.java
@@ -74,10 +74,12 @@ public class AddressBookTest {
      */
     private static class AddressBookStub implements ReadOnlyAddressBook {
         private final ObservableList<ReadOnlyPerson> persons = FXCollections.observableArrayList();
+        private final ObservableList<ReadOnlyPerson> favouritePersons = FXCollections.observableArrayList();
         private final ObservableList<Tag> tags = FXCollections.observableArrayList();
 
         AddressBookStub(Collection<? extends ReadOnlyPerson> persons, Collection<? extends Tag> tags) {
             this.persons.setAll(persons);
+            this.favouritePersons.setAll(persons);
             this.tags.setAll(tags);
         }
 
@@ -85,6 +87,9 @@ public class AddressBookTest {
         public ObservableList<ReadOnlyPerson> getPersonList() {
             return persons;
         }
+
+        @Override
+        public ObservableList<ReadOnlyPerson>getFavouritePersonList() { return favouritePersons; }
 
         @Override
         public ObservableList<Tag> getTagList() {
