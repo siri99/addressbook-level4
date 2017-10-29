34474f1d8769b9c4a81926d8d4539b1631929a22
linus <penxumelb@gmail.com>
2017-10-29 17:38:34 +0800

    Updated cover image

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -66,9 +66,7 @@ public class MainApp extends Application {
 
         initLogging(config);
 
-
         model = initModelManager(storage, userPrefs);
-
 
         logic = new LogicManager(model);
 
