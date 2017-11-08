7f4f2dec5c0941a41a273350289091a8e7ab87ca
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2017-11-09 01:33:45 +0800

    refactor for travis

--- src/main/java/seedu/address/ui/AddWindow.java
+++ src/main/java/seedu/address/ui/AddWindow.java
@@ -1,6 +1,7 @@
 //@@author Jacob Vosburgh
 package seedu.address.ui;
 
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -16,8 +17,6 @@ import seedu.address.logic.Logic;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.logic.parser.exceptions.ParseException;
-
-import java.util.logging.Logger;
 
 /**
  * Controller for a help page
