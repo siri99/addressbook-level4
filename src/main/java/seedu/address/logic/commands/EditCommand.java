f3c8d699e1f9f2e218ac28772c43013ea66ada9c
Jacob Vosburgh <jacob_vosburgh@yahoo.com>
2017-11-08 22:13:32 +0800

    Update EditCommand.java

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -59,7 +59,7 @@ public class EditCommand extends UndoableCommand {
     public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
     public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
-    private final EditPersonDescriptor editPersonDescriptor
+    private final EditPersonDescriptor editPersonDescriptor;
     private final Index index;
 
     private static final java.util.logging.Logger logger = LogsCenter.getLogger(EditCommand.class);
